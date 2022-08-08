0b803b16f2429f85ae93c423ba7eacee8f98ffb0
Abel Junior <abel.costa@webjump.com.br>
2022-08-08 16:44:39 -0300

    feat: add a custom exception filter

---
+++ src/shared/infra/filters/http-exceptions/http.exception.filter.ts
@@ -0,0 +1,28 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  Logger,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+
+    const request = context.getRequest<Request>();
+    const response = context.getResponse<Response>();
+    const status = exception.getStatus();
+    const message = exception.getResponse();
+
+    Logger.error(`${status} - ${message} on path ${request.path}`);
+
+    response.status(status).json({
+      statusCode: status,
+      message,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
