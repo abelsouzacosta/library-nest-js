55fc6d80aca12c98b738c8508125307eb1eaef73
Abel Junior <abel.costa@webjump.com.br>
2022-08-08 16:48:44 -0300

    feat(authors): middleware
    apply middleware to routes

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AuthorsService } from './authors.service';
 import { AuthorsController } from './authors.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Author, AuthorSchema } from './entities/author.entity';
 import { AuthorsRepository } from './domain/repositories/authors.repository';
+import { AuthorNotFoundMiddleware } from './infra/middlewares/author-not-found.middleware';
 
 @Module({
   imports: [
@@ -12,4 +18,21 @@ import { AuthorsRepository } from './domain/repositories/authors.repository';
   controllers: [AuthorsController],
   providers: [AuthorsService, AuthorsRepository],
 })
-export class AuthorsModule {}
+export class AuthorsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthorNotFoundMiddleware).forRoutes(
+      {
+        path: '/authors/:id',
+        method: RequestMethod.GET,
+      },
+      {
+        path: '/authors/:id',
+        method: RequestMethod.PUT,
+      },
+      {
+        path: '/authors/:id',
+        method: RequestMethod.DELETE,
+      },
+    );
+  }
+}
