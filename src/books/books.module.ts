f07a19139699d1dfa6b1778d943abfe6aed20051
abelsouzacosta <abelsouzacosta@gmail.com>
2022-08-10 02:58:26 -0300

    feat(books): error handling
    improve error handling with middleware and custom exception

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Book, BookSchema } from './entities/book.entity';
 import { BooksRepository } from './domain/repositories/book.repository';
+import { BookNotFoundMiddleware } from './infra/middlewares/book-not-found.middleware';
 
 @Module({
   imports: [
@@ -12,4 +18,15 @@ import { BooksRepository } from './domain/repositories/book.repository';
   controllers: [BooksController],
   providers: [BooksService, BooksRepository],
 })
-export class BooksModule {}
+export class BooksModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(BookNotFoundMiddleware)
+      .forRoutes(
+        { path: '/books/:id', method: RequestMethod.GET },
+        { path: '/books/:id', method: RequestMethod.PUT },
+        { path: '/books/add_authors_to_book/:id', method: RequestMethod.PATCH },
+        { path: '/books/:id', method: RequestMethod.DELETE },
+      );
+  }
+}
