8c807d253b9bc69f5eb83f715c1af2873a6c66c1
Abel Junior <abel.costa@webjump.com.br>
2022-08-11 14:33:45 -0300

    refactor(categories): controller
    adds improvements to controller route status and validations

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -6,6 +6,10 @@ import {
   Param,
   Delete,
   Put,
+  UsePipes,
+  ValidationPipe,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
@@ -16,21 +20,27 @@ export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Post()
+  @UsePipes(ValidationPipe)
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoriesService.create(createCategoryDto);
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   findAll() {
     return this.categoriesService.findAll();
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('id') id: string) {
     return this.categoriesService.findOne(id);
   }
 
   @Put(':id')
+  @UsePipes(ValidationPipe)
+  @HttpCode(HttpStatus.CREATED)
   update(
     @Param('id') id: string,
     @Body() updateCategoryDto: UpdateCategoryDto,
@@ -39,6 +49,7 @@ export class CategoriesController {
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
     return this.categoriesService.remove(id);
   }
