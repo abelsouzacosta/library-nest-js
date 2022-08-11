731bd4f54339af3e79fc98e77e933e9a035d5532
Abel Junior <abel.costa@webjump.com.br>
2022-08-11 13:12:44 -0300

    refactor(authors): controller
    adds improvements for validation

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -6,6 +6,10 @@ import {
   Param,
   Delete,
   Put,
+  UsePipes,
+  ValidationPipe,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { AuthorsService } from './authors.service';
 import { CreateAuthorDto } from './application/dto/create-author.dto';
@@ -16,26 +20,33 @@ export class AuthorsController {
   constructor(private readonly authorsService: AuthorsService) {}
 
   @Post()
+  @UsePipes(ValidationPipe)
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() data: CreateAuthorDto) {
     return this.authorsService.create(data);
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   findAll() {
     return this.authorsService.findAll();
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('id') id: string) {
     return this.authorsService.findOne(id);
   }
 
   @Put(':id')
+  @UsePipes(ValidationPipe)
+  @HttpCode(HttpStatus.CREATED)
   update(@Param('id') id: string, @Body() data: UpdateAuthorDto) {
     return this.authorsService.update(id, data);
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
     return this.authorsService.remove(id);
   }
