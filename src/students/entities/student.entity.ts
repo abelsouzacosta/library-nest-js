1f8eb339bbc1b319fc1f02b27588a0fff229de64
Abel Junior <abel.costa@webjump.com.br>
2022-08-15 09:59:08 -0300

    refactor(student): add model

--- src/students/entities/student.entity.ts
+++ src/students/entities/student.entity.ts
@@ -1,1 +1,24 @@
-export class Student {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({
+  timestamps: true,
+  collection: 'students',
+})
+export class Student {
+  @Prop({ required: true, type: String })
+  name: string;
+
+  @Prop({ required: true, type: String })
+  email: string;
+
+  @Prop({ required: true, type: Date })
+  date_birth: Date;
+
+  @Prop({ required: true, type: String })
+  ssn: string;
+
+  @Prop({ required: true, type: String })
+  register_number: number;
+}
+
+export const StudentSchema = SchemaFactory.createForClass(Student);
