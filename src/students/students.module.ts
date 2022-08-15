a41ee75be0dfa722323bfc88989fc20476803466
Abel Junior <abel.costa@webjump.com.br>
2022-08-15 14:34:32 -0300

    feat(students): middlewares
    adding middlewares for routes

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { StudentsController } from './students.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Student, StudentSchema } from './entities/student.entity';
 import { StudentsRepository } from './domain/repositories/students.repository';
+import { StudentNotFoundMiddleware } from './infra/middlewares/student-not-found.middleware';
+import { RegisterNumberAlreadyTakenMiddleware } from './infra/middlewares/register-number-already-taken.middleware';
+import { EmailAlreadyTakenMiddleware } from './infra/middlewares/email-already-taken.middleware';
+import { SsnAlreadyTakenMiddleware } from './infra/middlewares/ssn-already-taken.middleware';
 
 @Module({
   imports: [
@@ -12,4 +21,24 @@ import { StudentsRepository } from './domain/repositories/students.repository';
   controllers: [StudentsController],
   providers: [StudentsService, StudentsRepository],
 })
-export class StudentsModule {}
+export class StudentsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(StudentNotFoundMiddleware)
+      .forRoutes(
+        { path: 'students/:id', method: RequestMethod.GET },
+        { path: 'students/:id', method: RequestMethod.PUT },
+      );
+
+    consumer
+      .apply(
+        RegisterNumberAlreadyTakenMiddleware,
+        EmailAlreadyTakenMiddleware,
+        SsnAlreadyTakenMiddleware,
+      )
+      .forRoutes(
+        { path: 'students/', method: RequestMethod.POST },
+        { path: 'students/:id', method: RequestMethod.PUT },
+      );
+  }
+}
