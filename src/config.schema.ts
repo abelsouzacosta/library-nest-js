5ce2192cd98da3efc13cf608a1aac34f9c32f112
Abel Junior <abel.costa@webjump.com.br>
2022-08-16 14:19:17 -0300

    chore(application): config
    add field to validation

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -3,4 +3,5 @@ import * as Joi from 'joi';
 export const configValidationSchema = Joi.object({
   STAGE: Joi.string().required(),
   MONGO_URL: Joi.string().required(),
+  STUDENTS_UPLOAD_DIRECTORY: Joi.string().required(),
 });
