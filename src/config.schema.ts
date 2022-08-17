c27327e406758b437084710443e6b4debe904741
Abel Junior <abel.costa@webjump.com.br>
2022-08-17 20:52:06 -0300

    refactor(config): add environment variable to the configuration schema

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -4,4 +4,5 @@ export const configValidationSchema = Joi.object({
   STAGE: Joi.string().required(),
   MONGO_URL: Joi.string().required(),
   STUDENTS_UPLOAD_DIRECTORY: Joi.string().required(),
+  PASSWORD_HASH_SALT: Joi.number().required(),
 });
