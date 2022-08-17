6d573e2471d5d4f987312cbca05bf72fdf6d9367
Abel Junior <abel.costa@webjump.com.br>
2022-08-17 19:54:27 -0300

    chore(users): add repository implementation

---
+++ src/users/domain/repositories/user.repository.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from 'src/users/application/dto/create-user.dto';
+import { User } from 'src/users/entities/user.entity';
+import { IUserRepository } from 'src/users/infra/interfaces/user-repository.interface';
+
+@Injectable()
+export class UserRepository implements IUserRepository {
+  constructor(
+    @InjectModel(User.name)
+    private readonly model: Model<User>,
+  ) {}
+
+  async create(data: CreateUserDto): Promise<User> {
+    return this.model.create(data);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.model.findOne({
+      email,
+    });
+  }
+}
