dae2bc97d5d6becba5443d98c050a2ca6181910c
Abel Junior <abel.costa@webjump.com.br>
2022-08-17 20:32:36 -0300

    feat(users): create
    allows to create an user in the database

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './application/dto/create-user.dto';
-import { UpdateUserDto } from './application/dto/update-user.dto';
+import { UserRepository } from './domain/repositories/user.repository';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(private readonly repository: UserRepository) {}
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  create(data: CreateUserDto) {
+    return this.repository.create(data);
   }
 }
