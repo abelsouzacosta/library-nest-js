4a89f397d09c13ed831cbe8c32bfdf61ebb29a01
Abel Junior <abel.costa@webjump.com.br>
2022-08-17 20:50:55 -0300

    feat(users): create
    hashes the user password before insert in the database

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,13 +9,14 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './application/dto/create-user.dto';
+import { HashPasswordPipe } from './domain/pipes/hash-password.pipe';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  @UsePipes(ValidationPipe)
+  @UsePipes(ValidationPipe, HashPasswordPipe)
   @HttpCode(HttpStatus.CREATED)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
