d21822076febd0158732cbf879f499555ab1b705
Abel Junior <abel.costa@webjump.com.br>
2022-08-17 19:45:20 -0300

    refactor(users): adding user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,18 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({
+  timestamps: true,
+  collection: 'users',
+})
+export class User {
+  @Prop({ required: true, type: String })
+  name: string;
+
+  @Prop({ required: true, type: String, unique: true })
+  email: string;
+
+  @Prop({ required: true, type: String })
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
