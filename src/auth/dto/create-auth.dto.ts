1adc84459963f7fb4a69a4dead4ffe986090ba24
Abel Junior <abel.costa@webjump.com.br>
2022-08-18 10:56:36 -0300

    refactor(authentication): registering module

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,20 @@
-export class CreateAuthDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateAuthDto {
+  @IsString({
+    message: 'email should be a string',
+  })
+  @IsNotEmpty({
+    message: 'email should be provided',
+  })
+  @IsEmail()
+  email: string;
+
+  @IsString({
+    message: 'password should be a string',
+  })
+  @IsNotEmpty({
+    message: 'password should be provided',
+  })
+  password: string;
+}
