fa640d1722d1a19f8af4e509a240fd64f309c78f
Abel Junior <abel.costa@webjump.com.br>
2022-08-22 11:02:37 -0300

    refactor(books): middleware
    remove non used middleware

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,15 +1,9 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Book, BookSchema } from './entities/book.entity';
 import { BooksRepository } from './domain/repositories/book.repository';
-import { BookNotFoundMiddleware } from './infra/middlewares/book-not-found.middleware';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
@@ -20,15 +14,4 @@ import { AuthModule } from 'src/auth/auth.module';
   controllers: [BooksController],
   providers: [BooksService, BooksRepository],
 })
-export class BooksModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(BookNotFoundMiddleware)
-      .forRoutes(
-        { path: '/books/:id', method: RequestMethod.GET },
-        { path: '/books/:id', method: RequestMethod.PUT },
-        { path: '/books/add_authors_to_book/:id', method: RequestMethod.PATCH },
-        { path: '/books/:id', method: RequestMethod.DELETE },
-      );
-  }
-}
+export class BooksModule {}
