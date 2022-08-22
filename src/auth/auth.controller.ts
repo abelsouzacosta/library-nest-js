f05d31b944ba8c5812fb1955e091379adf6b7038
Abel Junior <abel.costa@webjump.com.br>
2022-08-22 11:38:10 -0300

    refactor(auth): controller
    binding pipe to controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,13 +9,14 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './application/dto/create-auth.dto';
+import { CheckUserExistsPipe } from './domain/pipes/check-user-exists.pipe';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post()
-  @UsePipes(ValidationPipe)
+  @UsePipes(ValidationPipe, CheckUserExistsPipe)
   @HttpCode(HttpStatus.CREATED)
   create(@Body() data: CreateAuthDto) {
     return this.authService.create(data);
