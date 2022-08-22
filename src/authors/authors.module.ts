b00819f31f1de342c5d6818c1c714361c4455969
Abel Junior <abel.costa@webjump.com.br>
2022-08-22 11:18:23 -0300

    refactor(authors): middleware
    remove non used middleware

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -1,15 +1,9 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AuthorsService } from './authors.service';
 import { AuthorsController } from './authors.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Author, AuthorSchema } from './entities/author.entity';
 import { AuthorsRepository } from './domain/repositories/authors.repository';
-import { AuthorNotFoundMiddleware } from './infra/middlewares/author-not-found.middleware';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
@@ -20,21 +14,4 @@ import { AuthModule } from 'src/auth/auth.module';
   controllers: [AuthorsController],
   providers: [AuthorsService, AuthorsRepository],
 })
-export class AuthorsModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthorNotFoundMiddleware).forRoutes(
-      {
-        path: '/authors/:id',
-        method: RequestMethod.GET,
-      },
-      {
-        path: '/authors/:id',
-        method: RequestMethod.PUT,
-      },
-      {
-        path: '/authors/:id',
-        method: RequestMethod.DELETE,
-      },
-    );
-  }
-}
+export class AuthorsModule {}
