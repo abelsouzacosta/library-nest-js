3a7dd497ca9127b22b95502d4fd149ef3fdbf25e
Abel Junior <abel.costa@webjump.com.br>
2022-08-22 10:27:52 -0300

    refactor(category): middleware
    remove non used middleware

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -9,7 +9,6 @@ import { CategoriesController } from './categories.controller';
 import { CategoryRepository } from './domain/repositories/category.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Category, CategorySchema } from './entities/category.entity';
-import { CategoryAlreadyExistsMiddleware } from './infra/middlewares/category-already-exists.middleware';
 import { CategoryNotFoundMiddleware } from './infra/middlewares/category-not-found.middleware';
 import { AuthModule } from 'src/auth/auth.module';
 
@@ -25,13 +24,6 @@ import { AuthModule } from 'src/auth/auth.module';
 })
 export class CategoriesModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(CategoryAlreadyExistsMiddleware)
-      .forRoutes(
-        { path: 'categories', method: RequestMethod.POST },
-        { path: 'categories/:id', method: RequestMethod.PUT },
-      );
-
     consumer
       .apply(CategoryNotFoundMiddleware)
       .forRoutes(
