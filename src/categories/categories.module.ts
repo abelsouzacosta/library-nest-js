03958983cd08e44a8fd930e8ff000a18bdef8a61
Abel Junior <abel.costa@webjump.com.br>
2022-08-22 12:49:57 -0300

    refactor(category): module
    register middleware to request, this addition was made only for test case

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 import { CategoryRepository } from './domain/repositories/category.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Category, CategorySchema } from './entities/category.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import { AuthenticationMiddleware } from 'src/shared/infra/middleware/authentication.middleware';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
@@ -12,8 +20,16 @@ import { AuthModule } from 'src/auth/auth.module';
       { name: Category.name, schema: CategorySchema },
     ]),
     AuthModule,
+    JwtModule,
+    UsersModule,
   ],
   controllers: [CategoriesController],
   providers: [CategoriesService, CategoryRepository],
 })
-export class CategoriesModule {}
+export class CategoriesModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthenticationMiddleware)
+      .forRoutes({ path: 'categories', method: RequestMethod.GET });
+  }
+}
