3d12ce905716d41f5ac8d8793bc1383e13af8625
Abel Junior <abel.costa@webjump.com.br>
2022-08-22 09:34:18 -0300

    refactor(users): controller
    binding pipe to controller route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,13 +10,14 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './application/dto/create-user.dto';
 import { HashPasswordPipe } from './domain/pipes/hash-password.pipe';
+import { CheckEmailAlreadyTakenPipe } from './domain/pipes/check-email-already-taken.pipe';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  @UsePipes(ValidationPipe, HashPasswordPipe)
+  @UsePipes(ValidationPipe, CheckEmailAlreadyTakenPipe, HashPasswordPipe)
   @HttpCode(HttpStatus.CREATED)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
