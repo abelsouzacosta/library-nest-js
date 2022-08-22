8c18fa3a8623064da4d2cce90fa8722316a1abef
Abel Junior <abel.costa@webjump.com.br>
2022-08-22 09:33:13 -0300

    refactor(students): middleware
    remove middleware from module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,16 +1,10 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UserRepository } from './domain/repositories/user.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.entity';
 import { ConfigModule } from '@nestjs/config';
-import { EmailAlreadyTakenMiddleware } from './infra/middlewares/email-already-taken.middleware';
 
 @Module({
   imports: [
@@ -21,10 +15,4 @@ import { EmailAlreadyTakenMiddleware } from './infra/middlewares/email-already-t
   providers: [UsersService, UserRepository],
   exports: [UserRepository],
 })
-export class UsersModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(EmailAlreadyTakenMiddleware)
-      .forRoutes({ path: 'users', method: RequestMethod.POST });
-  }
-}
+export class UsersModule {}
