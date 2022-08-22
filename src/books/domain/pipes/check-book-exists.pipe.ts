4a344cbf72b528eb99175af328cb298bf64f9373
Abel Junior <abel.costa@webjump.com.br>
2022-08-22 11:03:05 -0300

    refactor(books): pipe
    add a pipe that checks if an book exists in the database

---
+++ src/books/domain/pipes/check-book-exists.pipe.ts
@@ -0,0 +1,18 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { BookNotFoundException } from '../exceptions/book-not-found.exception';
+import { BooksRepository } from '../repositories/book.repository';
+
+@Injectable()
+export class CheckBookExistsPipe implements PipeTransform {
+  constructor(private readonly repository: BooksRepository) {}
+
+  async transform(value: any, metadata: ArgumentMetadata) {
+    if (metadata.type !== 'param') return value;
+
+    const book = await this.repository.findById(value);
+
+    if (!book) throw new BookNotFoundException(value);
+
+    return value;
+  }
+}
