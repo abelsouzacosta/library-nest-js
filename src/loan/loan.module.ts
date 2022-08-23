4e820e7cae6d35e428daa0e525ef7217bba7169a
Abel Junior <abel.costa@webjump.com.br>
2022-08-23 16:31:28 -0300

    feat(loan): database
    registering mongoose module for the module

--- src/loan/loan.module.ts
+++ src/loan/loan.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { LoanService } from './loan.service';
 import { LoanController } from './loan.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Loan, LoanSchema } from './entities/loan.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Loan.name, schema: LoanSchema }]),
+  ],
   controllers: [LoanController],
   providers: [LoanService],
 })
