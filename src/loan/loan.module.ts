beb77d7a1676e97a939d360aeac73937618974b5
Abel Junior <abel.costa@webjump.com.br>
2022-08-23 16:37:15 -0300

    refactor(laon): module
    registering repository as a provider

--- src/loan/loan.module.ts
+++ src/loan/loan.module.ts
@@ -3,12 +3,13 @@ import { LoanService } from './loan.service';
 import { LoanController } from './loan.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Loan, LoanSchema } from './entities/loan.entity';
+import { LoanRepository } from './domain/repositories/loan.repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Loan.name, schema: LoanSchema }]),
   ],
   controllers: [LoanController],
-  providers: [LoanService],
+  providers: [LoanService, LoanRepository],
 })
 export class LoanModule {}
