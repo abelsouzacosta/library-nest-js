f668242deb6b0291a5f7eaa6e3c71d600a150c45
Abel Junior <abel.costa@webjump.com.br>
2022-08-24 10:53:59 -0300

    refactor(loan): repository
    refactor repository interface

--- src/loan/infra/interfaces/loan-repository.interface.ts
+++ src/loan/infra/interfaces/loan-repository.interface.ts
@@ -1,10 +1,10 @@
 import { UpdateWriteOpResult } from 'mongoose';
-import { CreateLoanDto } from 'src/loan/application/dto/create-loan.dto';
 import { LoanRenewalDto } from 'src/loan/application/dto/loan-renewal.dto';
+import { SetLoanDTO } from 'src/loan/application/dto/set-loan.dto';
 import { Loan } from 'src/loan/entities/loan.entity';
 
 export interface ILoanRepository {
-  create(data: CreateLoanDto): Promise<Loan>;
+  create(data: SetLoanDTO): Promise<Loan>;
 
   find(): Promise<Array<Loan>>;
 
